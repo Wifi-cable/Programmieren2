1399b971570a88708e9c552948a874987b8b8067
wifi-cable <Wifi-cable@users.noreply.github.com>
2017-10-23 16:50:26 +0200

    erster commit von CompressingOutputStream, noch leerer file

---
+++ src/de/hsmannheim/inf/pr2/concurrent/CompressingOutputStream.java
@@ -0,0 +1,14 @@
+package de.hsmannheim.inf.pr2.concurrent;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class CompressingOutputStream extends FileOutputStream {
+
+	public CompressingOutputStream(File arg0) throws FileNotFoundException {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	// aufgabe 1.2 
+}
