a698823281798f93ac74941f39ed1986cafa96da
wifi-cable <Wifi-cable@users.noreply.github.com>
2017-10-24 12:27:54 +0200

    jetzt auch im korrekten pacakge. sorry

---
+++ src/de/hsmannheim/inf/pr2/io/compress/CompressingOutputStream.java
@@ -0,0 +1,17 @@
+package de.hsmannheim.inf.pr2.io.compress;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class CompressingOutputStream extends FileOutputStream {
+
+	public CompressingOutputStream(File arg0) throws FileNotFoundException {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	// aufgabe 1.2 
+	/*diese klasse soll die "Laufängen-Komprimierung" realisieren, (so etwas wie einen zip file bauen)
+	 * -sie erbt schon von fileoutputstream, wie im übungsblatt beschrieben. 
+	 * */
+}
