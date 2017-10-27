3dc83998e048cf2b8ce9f59b3a39991f609fe184
wifi-cable <Wifi-cable@users.noreply.github.com>
2017-10-27 16:56:58 +0200

    denkfehler behoben, bugs noch nicht

--- src/de/hsmannheim/inf/pr2/io/compress/CompressingOutputStream.java
+++ src/de/hsmannheim/inf/pr2/io/compress/CompressingOutputStream.java
@@ -1,14 +1,21 @@
 package de.hsmannheim.inf.pr2.io.compress;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 
-public class CompressingOutputStream extends FileOutputStream {
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
-	public CompressingOutputStream(File arg0) throws FileNotFoundException {	//braucht einen file als parameter. 
-		super(arg0);
-		//constructor stub
+public class CompressingOutputStream extends FilterOutputStream {
+
+	public CompressingOutputStream(OutputStream out) throws FileNotFoundException,IOException {
+		super(out);//braucht einen file als parameter. 
+		
+		
 	}
 	// aufgabe 1.2 
 	/*diese klasse soll die "Laufängen-Komprimierung" realisieren, (so etwas wie einen zip file bauen)
@@ -17,29 +24,21 @@ public class CompressingOutputStream extends FileOutputStream {
 	 * -sie benutzt zwei arrays. eins liest sie nur, das andere füllt sie mit kompremierten zahlen.*/
 	
 	// felder,  daten
+	File inputFile;
 	private byte[] rawData;
 	private byte[] zipData;
 	int fileLengh;
 	int rawIdx;//index des uncompremierten arrays
 	byte value;
 	
-	public CompressingOutputStream( byte [] input,String filePath) throws FileNotFoundException{
-		super(filePath);
+	public CompressingOutputStream( byte [] input,FilterOutputStream out) throws IOException{
+			super(out);
+			input=rawData;
+		
 	}
 	
-	/*hilfsmethode das array mit den kompremierten zahlen zu voll ist wird ein doppelt so grosses gebaut.
-	 * der inhalt des alten arrays wird ins neue kopiert.
-	 * die neue refernz wird auf die variable des alten arrays gesetzt- (aliasierung) */
-	public void resizeArray(){
-		int old=zipData.length;
-		int bigger=old*2;
-		byte []temp=new byte[bigger];
-		for(int i=0;i<old; i++){
-			temp[i]=zipData[i];
-			}
-		zipData= temp;
-		temp=null; // referenz löschen	
-	}
+
+	
 	public void compressArray(){
 		byte repeat;
 		byte next;
@@ -51,17 +50,15 @@ public class CompressingOutputStream extends FileOutputStream {
 			next=1;
 			if(rawData[rawIdx]==-1){		// sonderfall nächstes byte ist ein -1
 				if(zipIdx>=zipData.length-1){
-					resizeArray();
+					resizeArray(rawData);
 				}
 				zipData[zipIdx]=-1;
 				zipData[zipIdx+1]=-1;
 				zipIdx+=2;
 			}
 			else{				// nächstes byte ist keine -1
-				while(((rawIdx+next)<fileLengh)&& (value==rawData[next])){
-					repeat++;
-					// zip idx++???
-					
+				while(((rawIdx+next)<fileLengh)&& (value==rawData[next])){//wieviele wiederholungen der zahl gibts ?
+					repeat++;	
 				}
 				// wiederholt es sich?
 				
@@ -70,5 +67,48 @@ public class CompressingOutputStream extends FileOutputStream {
 		}
 		
 	}
-	
+	/*hilfsmethode das array mit den kompremierten zahlen zu voll ist wird ein doppelt so grosses gebaut.
+	 * der inhalt des alten arrays wird ins neue kopiert.
+	 * die neue refernz wird auf die variable des alten arrays gesetzt- (aliasierung) */
+	public void resizeArray(byte[]small){
+		
+		int old=small.length;
+		int bigger=old*2;
+		byte []temp=new byte[bigger];
+		for(int i=0;i<old; i++){
+			temp[i]=small[i];
+			}
+		small= temp;
+		temp=null; // referenz löschen	
+	}
+	// einfacher getter für die ein array mit kompremierten daten
+	public byte[] getZipData(){
+		return zipData;
+	}
+	//nimmt einen file und packt ihn in ein byte array eventuell sinvoller in aufgabe 1.3 zu packen
+//	protected byte[]fileToArray(File inFile){
+//		//int len= (int) inFile.length();
+//		int len=10, idx=0;
+//		byte[] arrayFromFile=new byte[len];
+//		try{
+//			InputStream in = new BufferedInputStream(new FileInputStream(inFile));
+//			int counter=  in.read(arrayFromFile);
+//			while(counter>0){
+//				if(idx<len){
+//				byte b= (byte)counter;
+//				arrayFromFile[idx]=b;
+//				idx++;
+//				}
+//				else{// geht net mit globalen arrays ohen parameter, oder? 
+//					resizeArray(arrayFromFile);
+//				}
+//				in.close();
+//			}
+//			
+//		}
+//		catch(IOException ioex){}
+//		
+//		return arrayFromFile;
+//	}
+
 }
--- src/de/hsmannheim/inf/pr2/io/compress/TestCompressingOutput.java
+++ src/de/hsmannheim/inf/pr2/io/compress/TestCompressingOutput.java
@@ -2,18 +2,56 @@ package de.hsmannheim.inf.pr2.io.compress;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.FilterOutputStream;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestCompressingOutput {
+	byte []tester1={1,2,4,-1,6,-1,-1,3};
+	byte []tester2= new byte[200];	//immer die selbe zahl
+	byte []tester3=new byte [50];	// random zahlen
+	byte[]tester4= {2,2,2,2,4,4,5,5,5,5,5,5,5,5,5,5};
+	CompressingOutputStream outPut;
+	File ex= new File("example");
+	OutputStream out;
 
 	@Before
 	public void setUp() throws Exception {
+		for (int i=0; i<200;i++){
+			tester2[i]=42;
+		}
+		for (int i=0; i<50;i++){
+			tester3[i]= (byte)((Math.random()*11)-2); // zahlen zwischen -2 und 9 
+		}
+		try{ // try catch weil outputstreams einem dauernt um die ohren fliegen. 
+			 outPut= new CompressingOutputStream(tester1, new FilterOutputStream( out));
+			}
+		catch(IOException inOutError){
+			inOutError.printStackTrace();
+		}
+		catch(Exception error){
+			error.printStackTrace();
+		}
+		
 	}
 
-	@Test
+	//@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
+	//@Test	// geht er mit 126 gleichen zahlen richtig um?
+	public void byteoverflow(){
+		
+	}
+	
+	//@Test // funst das mit den minus einsen?
+	public void negativeOne(){}
+	//@Test // tested wiederholungen
+	public void repetitions(){}
 
+
+	
 }
