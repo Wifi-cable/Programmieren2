88560e9d4155e7884fadb7f209d2b49e1abd21c4
Jiggyluf <33037200+Jiggyluf@users.noreply.github.com>
2017-11-11 18:47:12 +0100

    Add files via upload
    Schaue dir das bitte nochmal an, gib wenn es ist Verbesserungvorschläge und ob dies ansich so gedacht war und an einer Stelle wird eine NullPointerException geworfen, ob weitere Exceptions geworfen werden, weiß ich derzeitig nicht

--- src/de/hsmannheim/inf/pr2/concurrent/BaustellenSimulation.java
+++ src/de/hsmannheim/inf/pr2/concurrent/BaustellenSimulation.java
@@ -1,38 +1,89 @@
 package de.hsmannheim.inf.pr2.concurrent;
 
 public class BaustellenSimulation {
+	
 	int len;
+	
 	public BaustellenSimulation(int len){
+		
 		this.len=len;
 	}
 	//braucht ein strassen object. warscheinlich mit ampel objecten und auto objecten
 	
 	
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws SimulationsException {
+		
+		BaustellenSimulation laenge = new BaustellenSimulation(80);
+		
+		//Richtung rechts = Richtung.NACHRECHTS;
+		//Richtung links = Richtung.NACHLINKS;
+		
+		Strasse strassenbau = new Strasse(laenge.len);
+		
+		Runnable eins = strassenbau.setupAmpel(10, 1000, strassenbau);	
+		Runnable zwei = strassenbau.setupAmpel(30, 3000, strassenbau);
+		Runnable drei = strassenbau.setupAmpel(60, 3000, strassenbau);
+		Runnable vier = strassenbau.setupAmpel(70, 2000, strassenbau);
+		
+		Runnable VW = strassenbau.setupAuto(0, 10, Richtung.NACHRECHTS, strassenbau, 'A');		
+		Runnable Peugot = strassenbau.setupAuto(6, 12, Richtung.NACHRECHTS, strassenbau, 'B');
+		Runnable FIAT = strassenbau.setupAuto(79, 11, Richtung.NACHLINKS, strassenbau, 'C');
+		Runnable Audi = strassenbau.setupAuto(71, 10, Richtung.NACHLINKS, strassenbau, 'D');
+		Runnable Skoda = strassenbau.setupAuto(58, 8, Richtung.NACHLINKS, strassenbau, 'E');
+		
+		Thread ampel1 = new Thread(eins);
+		Thread ampel2 = new Thread(zwei);
+		Thread ampel3 = new Thread(drei);
+		Thread ampel4 = new Thread(vier);
+		
+		Thread auto1 = new Thread(VW);
+		Thread auto2 = new Thread(Peugot);
+		Thread auto3 = new Thread(FIAT);
+		Thread auto4 = new Thread(Audi);
+		Thread auto5 = new Thread(Skoda);
+		
+		//Vm sollte sich beenden, sobald alle Nicht Daemon Threads beendet wurden
+		ampel1.setDaemon(true);
+		ampel2.setDaemon(true);
+		ampel3.setDaemon(true);
+		ampel4.setDaemon(true);
+		
+		ampel1.start();
+		ampel2.start();
+		ampel3.start();
+		ampel4.start();
+		
+		auto1.start();
+		auto2.start();
+		auto3.start();
+		auto4.start();
+		auto5.start();
 
 	}
-	/*In einer Klasse BaustellenSimulation , die auch die main ()-Methode enthÃ¤lt, werden
-	nun die Fahrzeuge und Ampeln auf der StraÃ¿e positioniert und die Simulation wird
-	gestartet. Nach jeder VerÃ¤nderung des Zustandes soll auf der Konsole die StraÃ¿e mit
+	/*In einer Klasse BaustellenSimulation , die auch die main ()-Methode enthält, werden
+	nun die Fahrzeuge und Ampeln auf der Straÿe positioniert und die Simulation wird
+	gestartet. Nach jeder Veränderung des Zustandes soll auf der Konsole die Straÿe mit
 	den Ampeln und die Positionen der Fahrzeuge ausgegeben werden. Bei den Ampeln
 	wird durch ein > bzw. < angezeigt, dass die Fahrzeuge von links nach rechts bzw. von
-	rechts nach links fahren dÃ¼rfen. Eine beispielhafte Ausgabe sieht dann wie folgt aus:
+	rechts nach links fahren dürfen. Eine beispielhafte Ausgabe sieht dann wie folgt aus:
 	
 	*In diese beispielhaften Ausgabe warten die Fahrzeuge D, C und A gerade an einer
-	Ampel. Modellieren Sie die Ampeln und Fahrzeuge als Threads, die eigenstÃ¤ndig lau-
-   fen und Ã¼ber entsprechende Mittel der Threadkoordination miteinander interagieren.
-   Sorgen Sie dafÃ¼r, dass alle kritischen Abschnitte richtig geschÃ¼tzt sind und dass sich
+	Ampel. Modellieren Sie die Ampeln und Fahrzeuge als Threads, die eigenständig lau-
+   fen und über entsprechende Mittel der Threadkoordination miteinander interagieren.
+   Sorgen Sie dafür, dass alle kritischen Abschnitte richtig geschützt sind und dass sich
    die Threads sauber beenden, wenn das Programm zu Ende gelaufen ist. Die Simulation
-   ist dann beendet, wenn alle Fahrzeuge am Ende der StraÃ¿e angekommen sind. Rechnen
+   ist dann beendet, wenn alle Fahrzeuge am Ende der Straÿe angekommen sind. Rechnen
    Sie bei den Fahrzeugen die Geschwindigkeiten von km/h einfach eins-zu-eins in km/sec
- um, damit die Simulation hinreichend schnell ablÃ¤uft.*/
+ um, damit die Simulation hinreichend schnell abläuft.*/
 	
-	static int gibOrdinal( String ampelnummer ){	// hilfsmehtode ist die ampel rot oder grÃ¼n?
+	static int gibOrdinal( String ampelnummer ){	// hilfsmehtode ist die ampel rot oder grün?
+		
 	  try {
+		  
 	    return Richtung.valueOf( ampelnummer ).ordinal();
 	  }
 	  catch ( IllegalArgumentException e ) {
+		  
 	    return -1;
 	  }
 	}
