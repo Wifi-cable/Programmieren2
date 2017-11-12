9f4460b180477b075f49faa2a76144114233462d
Jiggyluf <33037200+Jiggyluf@users.noreply.github.com>
2017-11-12 16:21:11 +0100

    Add files via upload

--- src/de/hsmannheim/inf/pr2/concurrent/BaustellenSimulation.java
+++ src/de/hsmannheim/inf/pr2/concurrent/BaustellenSimulation.java
@@ -11,7 +11,7 @@ public class BaustellenSimulation {
 	//braucht ein strassen object. warscheinlich mit ampel objecten und auto objecten
 	
 	
-	public static void main(String[] args) throws SimulationsException, InterruptedException {
+	public static void main(String[] args) throws SimulationsException {
 		
 		BaustellenSimulation laenge = new BaustellenSimulation(80);
 		
@@ -60,35 +60,68 @@ public class BaustellenSimulation {
 		auto3.start();
 		auto4.start();
 		auto5.start();
-
-		auto1.join();
-		auto2.join();
-		auto3.join();
-		auto4.join();
-		auto5.join();
-		// debugg print statement
-		System.out.println("es ist nacht die strasse ist nass, kein auto f√§hrt mehr nur noch die ampeln schalten einsam");
 		
+		
+		// Je nach verwendetes Thread gibt es neue Ausgaben, die auch komplett anderes aussehen
+		// Hier werden die Ampeln sp‰ter erst ausgegeben, obwohl rein theoretisch jede Zeile durchgegangen wird
+		// Thread Auto 5 terminiert nicht
+		/*while (auto5.isAlive()) {
+		char anzeige[][] = strassenbau.getAnzeige();
+		
+			// Anzeige der oberenFahrbahn
+			for (int i = 0; i < 80; i++) {
+				System.out.print(anzeige[i][0]);
+			}
+			System.out.println();
+			
+			//Anzeige der mittlerenFahrbahn
+			for (int j = 0; j < 80; j++) {
+				System.out.print(anzeige[j][1]);
+			}
+			System.out.println();
+			
+			// Anzeige der unterenFahrbahn
+			for (int k = 0; k < 80; k++) {
+				System.out.print(anzeige[k][2]);
+			}
+			System.out.println();
+		
+		
+		}*/
+		
+		// Wird ein Ampel Thread verwendet, werden die Ampeln erst gegen Ende ausgegeben und alle Amplen bleiben im Zustand < und 
+		// das Thread verweilt auﬂerdem in einer Endlosschleife
+		/*while(ampel4.isAlive()) {
+			
+			char anzeige[][] = strassenbau.getAnzeige();
+			
+			for (int i = 0; i < 80; i++) {
+				for(int j = 0; j < 3; j++) {
+					System.out.print(anzeige[i][j]);
+				}
+			}
+			
+		}*/
 
 
 	}
-	/*In einer Klasse BaustellenSimulation , die auch die main ()-Methode enthÔøΩlt, werden
-	nun die Fahrzeuge und Ampeln auf der StraÔøΩe positioniert und die Simulation wird
-	gestartet. Nach jeder VerÔøΩnderung des Zustandes soll auf der Konsole die StraÔøΩe mit
+	/*In einer Klasse BaustellenSimulation , die auch die main ()-Methode enth‰lt, werden
+	nun die Fahrzeuge und Ampeln auf der Straˇe positioniert und die Simulation wird
+	gestartet. Nach jeder Ver‰nderung des Zustandes soll auf der Konsole die Straˇe mit
 	den Ampeln und die Positionen der Fahrzeuge ausgegeben werden. Bei den Ampeln
 	wird durch ein > bzw. < angezeigt, dass die Fahrzeuge von links nach rechts bzw. von
-	rechts nach links fahren dÔøΩrfen. Eine beispielhafte Ausgabe sieht dann wie folgt aus:
+	rechts nach links fahren d¸rfen. Eine beispielhafte Ausgabe sieht dann wie folgt aus:
 	
 	*In diese beispielhaften Ausgabe warten die Fahrzeuge D, C und A gerade an einer
-	Ampel. Modellieren Sie die Ampeln und Fahrzeuge als Threads, die eigenstÔøΩndig lau-
-   fen und ÔøΩber entsprechende Mittel der Threadkoordination miteinander interagieren.
-   Sorgen Sie dafÔøΩr, dass alle kritischen Abschnitte richtig geschÔøΩtzt sind und dass sich
+	Ampel. Modellieren Sie die Ampeln und Fahrzeuge als Threads, die eigenst‰ndig lau-
+   fen und ¸ber entsprechende Mittel der Threadkoordination miteinander interagieren.
+   Sorgen Sie daf¸r, dass alle kritischen Abschnitte richtig gesch¸tzt sind und dass sich
    die Threads sauber beenden, wenn das Programm zu Ende gelaufen ist. Die Simulation
-   ist dann beendet, wenn alle Fahrzeuge am Ende der StraÔøΩe angekommen sind. Rechnen
+   ist dann beendet, wenn alle Fahrzeuge am Ende der Straˇe angekommen sind. Rechnen
    Sie bei den Fahrzeugen die Geschwindigkeiten von km/h einfach eins-zu-eins in km/sec
- um, damit die Simulation hinreichend schnell ablÔøΩuft.*/
+ um, damit die Simulation hinreichend schnell abl‰uft.*/
 	
-	static int gibOrdinal( String ampelnummer ){	// hilfsmehtode ist die ampel rot oder grÔøΩn?
+	static int gibOrdinal( String ampelnummer ){	// hilfsmehtode ist die ampel rot oder gr¸n?
 		
 	  try {
 		  
