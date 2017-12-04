2a81f71fd67122b5903f5a5fefef86f50421e4da
wifi-cable <Wifi-cable@users.noreply.github.com>
2017-12-04 17:11:51 +0100

    korregierter test für das sortieren mit einem baum

--- src/de/hsmannheim/inf/pr2/coll/TestSortiertMitBaum.java
+++ src/de/hsmannheim/inf/pr2/coll/TestSortiertMitBaum.java
@@ -2,6 +2,8 @@ package de.hsmannheim.inf.pr2.coll;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -23,58 +25,87 @@ public class TestSortiertMitBaum {
 		
 		miniTree.add(16);
 		miniTree2.add(3);
-	
+		
+		miniTree.sort();
+		miniTree2.sort();
 		
 		biggerTree.add(8);
 		biggerTree.add(3);
 		biggerTree.add(16);	//bis hier gleich miniTree , miniTree2 und biggerTree2
 		biggerTree.add(2);
 		biggerTree.add(12);
+		biggerTree.sort();
 		
 		biggerTree2.add(8);
 		biggerTree2.add(3);
 		biggerTree2.add(16);	//bis hier gleich miniTree , miniTree2 und biggerTree1
 		biggerTree2.add(5);
 		biggerTree2.add(20);
+		biggerTree.sort();
 		nullTree.add(null);
+		nullTree.sort();
 		christmasTree.add("Ich bin ein Weihnachstsbaum. ");
+		christmasTree.sort();
 	}
 
 	@Test
 	public void testMini() {
-		assertEquals(miniTree.sortedList.get(0),3);
-		assertEquals(miniTree.sortedList.get(1),8);
-		assertEquals(miniTree.sortedList.get(2),16);
+		ArrayList <Integer>sortedList= miniTree2.sort();
+		assertEquals(sortedList.get(0),(Integer)3);
+		assertEquals(sortedList.get(1),(Integer)8);
+		assertEquals(sortedList.get(2),(Integer)16);
+		
+//		assertEquals(miniTree.sortedList.get(0),3);
+//		assertEquals(miniTree.sortedList.get(1),8);
+//		assertEquals(miniTree.sortedList.get(2),16);
 	}
 	@Test
 	public void testMini2() {
-		assertEquals(miniTree2.sortedList.get(0),3);
-		assertEquals(miniTree2.sortedList.get(1),8);
-		assertEquals(miniTree2.sortedList.get(2),16);
+		ArrayList <Integer>sortedList= miniTree2.sort();
+		assertEquals(sortedList.get(0),(Integer)3);
+		assertEquals(sortedList.get(1),(Integer)8);
+		assertEquals(sortedList.get(2),(Integer)16);
 	}
 	
 	@Test
 	public void testBiggerTree(){
-	assertEquals(biggerTree.sortedList.get(0),2);	
-	assertEquals(biggerTree.sortedList.get(1),3);
-	assertEquals(biggerTree.sortedList.get(2),8);
-	assertEquals(biggerTree.sortedList.get(3),12);
-	assertEquals(biggerTree.sortedList.get(4),16);
+	ArrayList <Integer>sortedList= biggerTree2.sort();		
+	assertEquals(sortedList.get(0),(Integer)2);	
+	assertEquals(sortedList.get(1),(Integer)3);
+	assertEquals(sortedList.get(2),(Integer)8);
+	assertEquals(sortedList.get(3),(Integer)12);
+	assertEquals(sortedList.get(4),(Integer)16);	
+	
+//	assertEquals(biggerTree.sortedList.get(0),2);	
+//	assertEquals(biggerTree.sortedList.get(1),3);
+//	assertEquals(biggerTree.sortedList.get(2),8);
+//	assertEquals(biggerTree.sortedList.get(3),12);
+//	assertEquals(biggerTree.sortedList.get(4),16);
 	}
 	@Test
 	public void testBiggerTree2(){
-	assertEquals(biggerTree2.sortedList.get(0),3);	
-	assertEquals(biggerTree2.sortedList.get(1),5);
-	assertEquals(biggerTree2.sortedList.get(2),8);
-	assertEquals(biggerTree2.sortedList.get(3),16);
-	assertEquals(biggerTree2.sortedList.get(4),20);
+	ArrayList <Integer>sortedList= biggerTree2.sort();	
+	assertEquals(sortedList.get(0),(Integer)3);	
+	assertEquals(sortedList.get(1),(Integer)5);
+	assertEquals(sortedList.get(2),(Integer)8);
+	assertEquals(sortedList.get(3),(Integer)16);
+	assertEquals(sortedList.get(4),(Integer)20);
+	
+//	assertEquals(biggerTree2.sortedList.get(0),3);	
+//	assertEquals(biggerTree2.sortedList.get(1),5);
+//	assertEquals(biggerTree2.sortedList.get(2),8);
+//	assertEquals(biggerTree2.sortedList.get(3),16);
+//	assertEquals(biggerTree2.sortedList.get(4),20);
 	}
 	@Test 
 	public void stringTree(){
-		assertEquals(christmasTree.sortedList.get(0),"Ich bin ein Weihnachstsbaum. ");
+		ArrayList<String>sortedList=christmasTree.sort();
+		assertEquals(sortedList.get(0),"Ich bin ein Weihnachstsbaum. ");
 	}
 	@Test
-	public void nullTest(){
-		assertNull(nullTree.sortedList.get(0),null);
+	public void nullTest(){	
+		ArrayList<String>sortedList=nullTree.sort();
+		assertNull(sortedList.get(0),null);
+		//assertNull(nullTree.sortedList.get(0),null);
 	}
 }
